ea3e716c1971095d3b8acc58a8b7fb6dddc9d02a
Bernice Tan <bernice.tann@gmail.com>
2017-05-25 19:44:25 +0000

    Made event-details handler

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,5 @@
 $(() => {
+
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -7,4 +8,30 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  //EVENT HANDLER: Display one container at a time until continue button is clicked.
+
+
+  //EVENT HANDLER: Make Continue button appear after 'Event Details' page is filled.
+  $(".event-details-form").
+
+  //EVENT HANDLER:
+  //When clicking the submit button after filling out 'Event Details' page,
+  //eventdetails container will toggle up.
+  $(".event-details-form").('submit', function (event) {
+    event.preventDefault();
+    $(".event-details").slidetoggle("fast", function() {
+      $.ajax({
+        method: 'POST',
+        url: '/',
+        data: $("form").serialize()
+      }).done();
+      // success(data => {
+      //   $tweetSection.prepend(createTweetElement(tweet));
+      // }).error(error => {
+      //   console.log('error', error);
+      // });
+    });
+  });
+
 });
