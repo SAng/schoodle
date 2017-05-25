136a8b1474aa3d10ad5c05e50cf7496b2e2a63ed
Bernice Tan <bernice.tann@gmail.com>
2017-05-25 22:47:30 +0000

    Updated changes to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,14 +7,17 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
 
-  //EVENT HANDLER: Display one container at a time until continue button is clicked.
+  //Display one container at a time until continue button is clicked.
 
 
   //EVENT HANDLER: Make Continue button appear after 'Event Details' page is filled.
-  $('.event-details-form').on('change' function() {
+  $('.event-details-form').on('change', function(event) {
+    event.preventDefault();
     var $eventTitle = $('.event-title').val();
+    $('.date-picker').hide();
+    $('.owner-info').hide();
     if($eventTitle !== "") {
       $('.submit-button').hide();
     }
@@ -26,10 +29,8 @@ $(() => {
   $('.submit-button').on('click', function(event) {
     $('.event-details').slideToggle("fast", function() {
     });
+    // $('.date-picker').show();
   });
-
-
-
 
 
 });
