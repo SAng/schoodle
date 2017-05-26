341d52de5c1fa66d031c32a05ab7df0bb77b80ee
Bernice Tan <bernice.tann@gmail.com>
2017-05-26 19:10:15 +0000

    Added button to add participants to table slots

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,9 +9,6 @@ $(() => {
   //   }
   // });
 
-  //Display one container at a time until continue button is clicked.
-
-
   //EVENT HANDLER:
   //When clicking the submit button after filling out 'Event Details' page,
   //eventdetails container will toggle up.
@@ -22,17 +19,32 @@ $(() => {
     $('.date-picker').show();
   });
 
-  $('.add-date-button').on('click',function(e){
+  //EVENT HANDLER: Click Add date button to add new date and will append
+  //date picker.
+  $('.add-date-button').on('click', function(e) {
     e.preventDefault();
     var $datePicker = $("<input type='date'>").addClass("date-picker");
     var $startTime = $("<input type='time'>").addClass("start-time");
     var $endTime = $("<input type='time'>").addClass("end-time");
+    var $deleteButton = $("<input type='button'>").addClass("delete-date-button");
     var $form = $("<form>").addClass("date-picker-form");
-    $form.append( $datePicker, $startTime, $endTime );
+
+    $form.append( $datePicker, $startTime, $endTime, $deleteButton );
     $('.event-date-time').append( $form );
   });
 
+  //EVENT HANDLER: Toggles the event-details down and hides date picker container.
+  $('.toggle-event-details').on('click', function(e) {
+    e.preventDefault();
+    $('.event-details').slideToggle("fast", function() {
+    });
+    $('.date-picker').hide();
+  });
 
+  $('.delete-date-button').on('click', function(e) {
+    e.preventDefault();
+    $(this).remove();
+  });
 
 
 
