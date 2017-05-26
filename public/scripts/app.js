6605aa2dcc27f4337bc1275df71ca1869368bf09
Bernice Tan <bernice.tann@gmail.com>
2017-05-26 05:19:33 +0000

    Added a button to add more dates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,42 @@
 $(() => {
 
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  //   }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });
 
   //Display one container at a time until continue button is clicked.
 
 
-  //EVENT HANDLER: Make Continue button appear after 'Event Details' page is filled.
-  // $('.event-details-form').on('change', function(event) {
-  //   event.preventDefault();
-  //   var $eventTitle = $('.event-title').val();
-  //   // $('.date-picker').hide();
-  //   // $('.owner-info').hide();
-  //   if($eventTitle !== "") {
-  //     $('.submit-button').hide();
-  //   }
-  // });
-
   //EVENT HANDLER:
   //When clicking the submit button after filling out 'Event Details' page,
   //eventdetails container will toggle up.
-  $('.submit-button').on('click', function(event) {
+  $('.continue-button').on('click', function(e) {
+    e.preventDefault();
     $('.event-details').slideToggle("fast", function() {
     });
-     $('.date-picker').show();
+    $('.date-picker').show();
   });
 
-  $('#datetimepicker12').datetimepicker({
-      inline: true,
-      sideBySide: true
-
+  $('.add-date-button').on('click',function(e){
+    e.preventDefault();
+    var $datePicker = $("<input type='date'>").addClass("date-picker");
+    var $startTime = $("<input type='time'>").addClass("start-time");
+    var $endTime = $("<input type='time'>").addClass("end-time");
+    var $form = $("<form>").addClass("date-picker-form");
+    $form.append( $datePicker, $startTime, $endTime );
+    $('.event-date-time').append( $form );
   });
+
+
+
+
+
+
 
 
 });
