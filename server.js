4505d1dc2c42dc8261f0ac1544bc1068502f9f96
sevenveils <hghamilton@gmail.com>
2017-05-27 21:55:51 +0000

    Modified server
    changed routes for event info form, wrote knex queries to check to see if event owner already exists in the database and modified forms so names match columns in users tables and events tables.

--- server.js
+++ server.js
@@ -39,17 +39,68 @@ app.use(express.static("public"));
 app.use("/api/users", usersRoutes(knex));
 
 // Home page
-//app.get("/", (req, res) => {
-  //res.render("index");
-//});
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Home page
 app.get("/events", (req, res) => {
   res.render("create_events");
 });
 
-// Event Creation
-app.post("/events", (req, res) => {
+// Event Creation (1 of 2)
+// 2 app posts. one to create event and add time slots so we don't need if/else statements to sort through data
+app.post("/create-event", (req, res) => {
+  console.log(req.body);
+
+  const userData = {
+    name: req.body.name
+  };
+
+  let userId = "";
+  let userFound = false;
+
+  console.log("Searching database to see if user already exists...");
+    // get list of users (knex docs for "knex select all rows from a table")
+  knex.select("*").from("users").then(function(response) {
+
+    //console.log("OBJ: " + obj);
+    //console.log("BUILDER: " + builder);
+
+    // check if the user already exists (check if req.body.name is in list)
+    response.forEach(function(userObj) {
+      if (req.body.name === userObj.name) {
+        // if it does, save its id (from the list of users you got from the db)
+        userId = userObj.id;
+        userFound = true;
+        console.log("This user exists already");
+      }
+    });
+
+    // IF it doesn't, insert it (use this function), and save its id
+    if (!userFound) {
+      knex.insert(userData).into("users").then(function (id) {
+        userId = id;
+        console.log("We found a match! " + id);
+      })
+    }
+
+    const eventData = {
+      title: req.body.title,
+      description : req.body.description,
+      owner_id: userId
+    };
+    console.log(eventData);
+    // write event, with the user's id
+    knex.insert(eventData).into("events").then(function (id) {
+      res.render(id);
+    });
+  });
+});
+
+
+// Event Creation (2 of 2). This will add time slots to database.
+app.post("/add-time-slots", (req, res) => {
   console.log(req.body);
 
 });
