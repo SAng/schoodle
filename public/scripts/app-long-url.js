20ee8b36a964e66b8ba57fd21ab534cca68fd971
Bernice Tan <bernice.tann@gmail.com>
2017-05-27 04:07:02 +0000

    Passed in variables so that title, description and slots would be the user's

--- public/scripts/app-long-url.js
+++ public/scripts/app-long-url.js
@@ -12,24 +12,48 @@ $(() => {
     var $row = $("<tr>").addClass("row");
     var $buttonColumn = $("<td>").addClass("button-column")
     var $participantColumn = $("<td>").addClass("participant-column");
-    var $addParticipantBtn = $("<button> + </button>").addClass("add-participant-button");
-    var $deleteParticipantBtn = $("<button> - </button>").addClass("delete-participant");
+    var $editParticipantBtn = $("<button> Edit </button>").addClass("edit-participant");
+    var $submitButton = $("<input type='submit' value='Submit'>").addClass("submit-button");
+    var $deleteParticipantBtn = $("<button> Delete </button>").addClass("delete-participant");
     var $div = $("<div>").addClass("add-participant");
     var $textBox = $("<input type='text'>").addClass("add-participant-text");
 
-    $buttonColumn.append( $addParticipantBtn, $deleteParticipantBtn );
+    $buttonColumn.append( $editParticipantBtn, $deleteParticipantBtn, $submitButton );
     $participantColumn.append( $textBox );
     $row.append( $buttonColumn, $participantColumn );
     $('.table-slot').append( $row );
 
   });
 
+   //Deletes a row from the table
+  $('.table-slot').on('click', '.delete-participant', function(e) {
+    e.preventDefault();
+    $(this).closest('tr').remove();
+  });
+
+
   //Copy button on long URL
-  $()
+
 
   //When clicking Edit, make participant text box be able to type
 
+
   //POST request when someone add themselves as participant
+  $('.submit-button').on('click', function(e) {
+    e.preventDefault();
+    if() {
+      else
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/:long_url/users/:userid',
+        data: $(this).serialize()
+      }).success(tweet => {
+        $tweetSection.prepend(createTweetElement(tweet));
+      }).error(error => {
+        console.log('error', error);
+      });
+  });
 
   //SUBMIT to add participants/time slots to database
 
