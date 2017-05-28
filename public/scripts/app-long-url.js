1412e6a141e1081521a7957d256fe3961406f4e9
Bernice Tan <bernice.tann@gmail.com>
2017-05-28 02:23:01 +0000

    Styled header

--- public/scripts/app-long-url.js
+++ public/scripts/app-long-url.js
@@ -1,29 +1,35 @@
 
 
 $(() => {
-  //Edit/Delete Button beside each user
 
   //Adds participants to table
   $('.add-participant-button').on('click', function (e) {
     event.preventDefault();
-    // $(this).hide();
-    $('.add-participant').show();
     // appends another row for more participants
     var $row = $("<tr>").addClass("row");
-    var $buttonColumn = $("<td>").addClass("button-column")
+    var $buttonColumn = $("<td>").addClass("button-column");
     var $participantColumn = $("<td>").addClass("participant-column");
     var $editParticipantBtn = $("<button> Edit </button>").addClass("edit-participant");
     var $submitButton = $("<input type='submit' value='Submit'>").addClass("submit-button");
     var $deleteParticipantBtn = $("<button> Delete </button>").addClass("delete-participant");
     var $div = $("<div>").addClass("add-participant");
     var $textBox = $("<input type='text'>").addClass("text-box");
+    var $td = $("<td>").addClass("check-box");
+    var $checkBoxRow = $td.append('<i class="fa fa-circle-o" aria-hidden="true">');
 
     $buttonColumn.append( $editParticipantBtn, $deleteParticipantBtn, $submitButton );
     $participantColumn.append( $textBox );
-    $row.append( $buttonColumn, $participantColumn );
+    $row.append( $buttonColumn, $participantColumn, $checkBoxRow );
     $('.table-slot').append( $row );
 
   });
+
+  function dateExists() {
+    if($(th)) {
+      $("<td>").addClass("check-box");
+    }
+
+  }
 
    //Deletes a row from the table
   $('.table-slot').on('click', '.delete-participant', function(e) {
@@ -31,17 +37,21 @@ $(() => {
     $(this).closest('tr').remove();
   });
 
-  //Get data from main_page to render on to long-url page
+  //Makes icons clickable
 
   //Copy button on long URL
 
 
   //When clicking Edit, make participant text box be able to type
-  $('.edit-participant').on('click', function (e) {
+  $('.table-slot').on('click', '.edit-participant', function(e) {
     e.preventDefault();
-    $('.participant-name').hide();
-    // $('.table-slot').find('.row').closest('.text-box').show();
-    $('.text-box').show();
+    $(this).closest('tr').find('text-box').show();
+
+
+    // $('.fa.fa-circle-o').on('click', function(e) {
+    //   $('.fa.fa-check-circle').toggle();
+    // });
+
   });
 
   //POST request when someone add themselves as participant
